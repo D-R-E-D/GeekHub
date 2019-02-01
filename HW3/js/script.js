bece1ca22f33d0e9d262dfa3dd777c3036e59f58
D-R-E-D <shut.oleg4@gmail.com>
2019-02-01 13:55:01 +0200

    Attached items to slides

--- HW3/js/script.js
+++ HW3/js/script.js
@@ -4,22 +4,49 @@ const BTN_NEXT = document.querySelector('.next');
 const BTN_PREV = document.querySelector('.prev');
 let i = 0;
 
+
 BTN_NEXT.addEventListener('click', () => {
   IMAGES[i].style.display = 'none';
+  ITEMS[i].classList.remove('item-active');
   i++;
   if (i >= IMAGES.length) i = 0;
   IMAGES[i].style.display = 'block';
+  ITEMS[i].classList.add('item-active');
 });
 
 BTN_PREV.addEventListener('click', () => {
   IMAGES[i].style.display = 'none';
+  ITEMS[i].classList.remove('item-active');
   i--;
   if (i < 0) i = IMAGES.length - 1;
   IMAGES[i].style.display = 'block';
+  ITEMS[i].classList.add('item-active');
 });
 
 
 // Мої експеременти, компії...
+
+
+// for (let item of ITEMS) {
+//   item.addEventListener('click', () => {
+//     item.classList.add('item-active');
+//   });
+// }
+
+
+// activate(ITEMS, i);
+// activate(IMAGES, i);
+
+// function activate(items, k) {
+//   for (let i = 0; i < items.length; i++) {
+//     const item = items[i];
+//     if (i === k) {
+//       item.classList.add('item-active');
+//     } else {
+//       item.classList.remove('item-active');
+//     }
+//   }
+// }
 
 /*
 document.addEventListener('DOMContentLoaded', () => {
